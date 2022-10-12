0e4af37540a089612d577190c2e534a8e23fabd7
The Kicker <cav.lemasters@geneva.edu>
2022-10-12 14:35:38 -0400

    Base file is ready

--- EXCEL/orders.js
+++ EXCEL/orders.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const pt = require('path');
-const csv = require('csv-parser');
 var readline = require('readline');
 
 //joining path of directory 
 const downloads = require('path').join(require('os').homedir(), 'Downloads')
 
 // Make an array of CSV files 
+const csv = require('csv-parser');
 var fileExt = ".csv"
-var csvFiles = []
+var filesArray = []
 // passing directoryPath and callback function
 fs.readdir(downloads, function (err, files) {
     //handling error
@@ -20,30 +20,29 @@ fs.readdir(downloads, function (err, files) {
         // check for file extension, if it's a valid CSV then continue
         if(pt.extname(file) == fileExt){
             // console.log(pt.basename(file), " is a file")
-            csvFiles.push(pt.basename(file))
+            filesArray.push(pt.basename(file))
         } else {
             console.log(pt.basename(file), " is not a", fileExt)
         }
     });
 
     // Log the options
-    console.log(csvFiles)
+    console.log(filesArray)
 
-    if(csvFiles.length === 0){
-        console.log("ERR: There are no CSV files found in", downloads)
+    if(filesArray.length === 0){
+        console.log("ERR: There are no ", fileExt ," files found in", downloads)
     } else {
         // Ask the user to select the index of a file from your array
         var rl = readline.createInterface({ input: process.stdin, output: process.stdout});
         rl.question("Please select the index of your file: ", (f)=> {
             var choice = parseInt(f)
-            console.log(choice)
             if( Number.isInteger(choice)){
-                if( choice > csvFiles.length || choice < 0){
+                if( choice > filesArray.length || choice < 0){
                     console.log("ERR: You entered a value outside of the index of files in the array. ")
                 } else {
-                    // Do stuff
-                    console.log(csvFiles[choice], "was chosen.")
-                    
+                    // Customize myFunction below to do stuff
+                    const filePath = require('path').join(require('os').homedir(), 'Downloads', filesArray[choice]) 
+                    myFunction(filePath)
                 }
             } else {
                 console.log("ERR: You did not enter a valid number for the index!  ")
@@ -52,3 +51,18 @@ fs.readdir(downloads, function (err, files) {
         });
     }
 });
+
+function myFunction(path){
+    console.log(path)
+    const results = []
+
+    fs.createReadStream(path)
+        .pipe(csv({}))
+        .on('data', (data)=>{ results.push(data)})
+        .on('end', ()=>{
+            // Do stuff
+            results.forEach(element => {
+                console.log(element.Vendor)
+            });
+        })
+}
