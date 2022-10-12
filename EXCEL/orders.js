484666e0f8e4b4a9992ab002acda1046835730e6
The Kicker <cav.lemasters@geneva.edu>
2022-10-12 13:35:47 -0400

    Changed default folder to users DOWNLOADS folder

--- EXCEL/orders.js
+++ EXCEL/orders.js
@@ -3,14 +3,13 @@ const pt = require('path');
 var readline = require('readline');
 
 //joining path of directory 
-const directoryPath = pt.join(__dirname, '..', 'assets');
-// console.log(directoryPath)
+const downloads = require('path').join(require('os').homedir(), 'Downloads')
 
 // Make an array of CSV files 
 var fileExt = ".csv"
 var csvFiles = []
 // passing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
+fs.readdir(downloads, function (err, files) {
     //handling error
     if (err) {
         return console.log('ERR: Unable to scan directory. --' + err);
@@ -25,23 +24,29 @@ fs.readdir(directoryPath, function (err, files) {
             console.log(pt.basename(file), " is not a", fileExt)
         }
     });
+
+    // Log the options
     console.log(csvFiles)
 
-    // Ask the user to select the index of a file from your array
-    var rl = readline.createInterface({ input: process.stdin, output: process.stdout});
-    rl.question("Please select the index of your file: ", (f)=> {
-        var choice = parseInt(f)
-        console.log(choice)
-        if( Number.isInteger(choice)){
-            if( choice > csvFiles.length || choice < 0){
-                console.log("ERR: You entered a value outside of the index of files in the array. ")
+    if(csvFiles.length === 0){
+        console.log("ERR: There are no CSV files found in", downloads)
+    } else {
+        // Ask the user to select the index of a file from your array
+        var rl = readline.createInterface({ input: process.stdin, output: process.stdout});
+        rl.question("Please select the index of your file: ", (f)=> {
+            var choice = parseInt(f)
+            console.log(choice)
+            if( Number.isInteger(choice)){
+                if( choice > csvFiles.length || choice < 0){
+                    console.log("ERR: You entered a value outside of the index of files in the array. ")
+                } else {
+                    // Do stuff
+                    console.log(csvFiles[choice], "was chosen.")
+                }
             } else {
-                // Do stuff
-                console.log(csvFiles[choice], "was chosen.")
+                console.log("ERR: You did not enter a valid number for the index!  ")
             }
-        } else {
-            console.log("ERR: You did not enter a valid number for the index!  ")
-        }
-        rl.close();
-    });
+            rl.close();
+        });
+    }
 });
